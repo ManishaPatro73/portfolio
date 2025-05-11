5dd5b5957187eb0b8258189e3b2a9d0ce9517526
Mohak Waghchaure <mohak11jan@gmail.com>
2025-05-11 13:56:58 -0400

    popup updates

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,44 +1,95 @@
-import EducationImage from '../image/educationImage.jpg';
+import { useState } from "react";
+import EducationImage from "../image/educationImage.jpg";
+import ArrowRightIcon from "../image/icons/top-right.png";
+import PopUp from "./PopUp";
 
 const Education = () => {
-    return (
-        <section id={'education'} className="w-full flex items-center justify-center">
-            <div className="container w-full flex items-center justify-between px-[100px] py-[100px]">
-                <div className="text-left w-[50%]">
-                    <h2 className="text-2xl font-bold pb-10">Experience</h2>
-                    <div>
-                        <div className="pb-5 cursor-pointer">
-                            <div className="text-xl font-semibold">Consumer Insights Analyst</div>
-                            <div className="text-xl font-normal">Extern</div>
-                            <div className="text-xl font-thin">New york</div>
-                            <div className="text-xl font-thin">Jan 2025 to present</div>
-                        </div>
-                        <div className="pb-5 cursor-pointer">
-                            <div className="text-xl font-semibold">Consumer Insights Analyst</div>
-                            <div className="text-xl font-normal">Extern</div>
-                            <div className="text-xl font-thin">New york</div>
-                            <div className="text-xl font-thin">Jan 2025 to present</div>
-                        </div>
-                        <div className="pb-5 cursor-pointer">
-                            <div className="text-xl font-semibold">Consumer Insights Analyst</div>
-                            <div className="text-xl font-normal">Extern</div>
-                            <div className="text-xl font-thin">New york</div>
-                            <div className="text-xl font-thin">Jan 2025 to present</div>
-                        </div>
-                        <div className="pb-5 cursor-pointer">
-                            <div className="text-xl font-semibold">Consumer Insights Analyst</div>
-                            <div className="text-xl font-normal">Extern</div>
-                            <div className="text-xl font-thin">New york</div>
-                            <div className="text-xl font-thin">Jan 2025 to present</div>
-                        </div>
-                    </div>
+  const [openEduPopUp, setOpenEduPopUp] = useState(false);
+  // const [popUpContent, setPopUpContent] = useState('');
+  const openPopUp = () => {
+    setOpenEduPopUp(true);
+  };
+  const closePopUp = () => {
+    setOpenEduPopUp(false);
+  };
+  return (
+    <section
+      id={"education"}
+      className="w-full flex items-center justify-center"
+    >
+      <div className="container w-full flex items-center justify-between px-[100px] py-[100px]">
+        <div className="text-left w-[50%]">
+          <h2 className="text-2xl font-bold pb-10">Education</h2>
+          <div>
+            <div
+              className="pb-5 flex items-start gap-[10px] cursor-pointer w-fit group"
+              onClick={openPopUp}
+            >
+              <div>
+                <div className="text-xl font-semibold">
+                  Consumer Insights Analyst
                 </div>
-                <div className="flex w-[50%]">
-                    <img src={EducationImage} width={650}></img>
-                </div>
+                <div className="text-xl font-normal">Extern</div>
+                <div className="text-xl font-thin">New york</div>
+                <div className="text-xl font-thin">Jan 2025 to present</div>
+              </div>
+              <div className="p-[10px] opacity-0 group-hover:opacity-100 ease-in duration-100">
+                <img
+                  src={ArrowRightIcon}
+                  height={12}
+                  width={12}
+                  alt="ArrowRightIcon"
+                />
+              </div>
             </div>
-        </section>
-    )
-}
+            <div
+              className="pb-5 flex items-start gap-[10px] cursor-pointer w-fit group"
+              onClick={openPopUp}
+            >
+              <div>
+                <div className="text-xl font-semibold">
+                  Consumer Insights Analyst
+                </div>
+                <div className="text-xl font-normal">Extern</div>
+                <div className="text-xl font-thin">New york</div>
+                <div className="text-xl font-thin">Jan 2025 to present</div>
+              </div>
+              <div className="p-[10px] opacity-0 group-hover:opacity-100 ease-in duration-100">
+                <img
+                  src={ArrowRightIcon}
+                  height={12}
+                  width={12}
+                  alt="ArrowRightIcon"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex w-[50%]">
+          <img src={EducationImage} width={650} alt="EducationImage"></img>
+        </div>
+      </div>
+      <PopUp openPopUp={openEduPopUp} closePopUp={closePopUp}>
+        <div>
+          <div className="text-left px-[20px] py-[20px]">
+            <div className="text-xl font-semibold pb-[5px]">
+              Consumer Insights Analyst
+            </div>
+            <div className="text-xl font-normal pb-[5px]">Extern</div>
+            <div className="text-xl font-thin pb-[5px]">New york</div>
+            <div className="text-xl font-thin pb-[5px]">Jan 2025 to present</div>
+            <div>
+                <div>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</div>
+                <div>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</div>
+                <div>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</div>
+                <div>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</div>
+                <div>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</div>
+            </div>
+          </div>
+        </div>
+      </PopUp>
+    </section>
+  );
+};
 
 export default Education;
--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -1,15 +1,14 @@
 import { useState } from "react";
+import CloseIcon from "../image/icons/close-icon.png";
 
-const PopUp = () =>{
+const PopUp = ({openPopUp, closePopUp, children }) =>{
 const [showPopUp, setShowPopUp] = useState(false);
-
-    const handlePopUp = () =>{
-        setShowPopUp(!showPopUp);
-    }
+    
     return(
-        <div className={`fixed top-0 left-0 h-[100vh] w-[100vw] flex items-center justify-center bg-colorFour/50 ${!showPopUp && 'hidden'}`}>
+        <div className={`fixed top-0 left-0 h-[100vh] w-[100vw] flex items-center justify-center bg-colorFour/50 ${!openPopUp && 'hidden'}`}>
             <div className="bg-colorTwo h-[500px] w-[700px] relative z-100">
-                <div className="absolute right-[5px] top-[5px] cursor-pointer" onClick={handlePopUp}>Close pop up</div>
+                <div className="absolute right-[10px] top-[10px] cursor-pointer" onClick={closePopUp}><img src={CloseIcon} alt="CloseIcon" height={10} width={10}/></div>
+                {children}
             </div>
         </div>
     )
